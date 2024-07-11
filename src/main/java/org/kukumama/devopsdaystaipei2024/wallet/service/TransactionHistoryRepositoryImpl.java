1675dff39f8aa32f5f7a96bc20f9567bc8f3a7a6
Roger Lo <sobadrush@icloud.com>
2024-07-11 10:53:42 +0800

    [交易紀錄] 使用 static map 模擬交易紀錄 database 操作

--- src/main/java/org/kukumama/devopsdaystaipei2024/wallet/service/TransactionHistoryRepositoryImpl.java
+++ src/main/java/org/kukumama/devopsdaystaipei2024/wallet/service/TransactionHistoryRepositoryImpl.java
@@ -2,16 +2,22 @@ package org.kukumama.devopsdaystaipei2024.wallet.service;
 
 import org.kukumama.devopsdaystaipei2024.wallet.entity.TransactionHistory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TransactionHistoryRepositoryImpl implements TransactionHistoryRepository {
+
+    private static Map<Long, TransactionHistory> histories = new HashMap<>();
+
+
     @Override
     public Map<Long, TransactionHistory> getTransactionHistories() {
-        return Map.of();
+        return histories;
     }
 
     @Override
     public int addTransaction(TransactionHistory wallet) {
-        return 0;
+        histories.put(wallet.getId(), wallet);
+        return 1;
     }
 }
