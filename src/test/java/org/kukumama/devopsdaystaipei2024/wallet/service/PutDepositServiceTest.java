4333bbd4138162363fb1d80dc6533295b3e8f1c2
Yu-Song Syu <kuma@cloud-latitude.com>
2024-07-17 22:35:10 +0800

    back to init

--- src/test/java/org/kukumama/devopsdaystaipei2024/wallet/service/PutDepositServiceTest.java
+++
@@ -1,9 +0,0 @@
-package org.kukumama.devopsdaystaipei2024.wallet.service;
-
-import org.junit.jupiter.api.Test;
-
-class PutDepositServiceTest {
-    @Test
-    void deposit_successfully() {
-    }
-}
