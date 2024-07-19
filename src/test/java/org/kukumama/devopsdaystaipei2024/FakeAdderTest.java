c51f1faf6c7337ce08b5a26b5744943f73452498
Eric Chiang <erichan0827@gmail.com>
2024-07-19 20:10:27 +0800

    new code

--- src/test/java/org/kukumama/devopsdaystaipei2024/FakeAdderTest.java
+++ src/test/java/org/kukumama/devopsdaystaipei2024/FakeAdderTest.java
@@ -1,6 +1,7 @@
 package org.kukumama.devopsdaystaipei2024;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class FakeAdderTest {
@@ -8,5 +9,12 @@ class FakeAdderTest {
     @Test
     void name() {
         Assertions.assertThat(new FakeAdder().add(1, 1)).isEqualTo(2);
+    }
+
+    @Test
+    @Disabled
+    void logExist(){
+        String[] args = new Command().parse("run-server -l");
+        Assertions.assertThat(args.length).isEqualTo(1);
     }
 }
